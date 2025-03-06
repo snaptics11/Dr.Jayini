ddf5c9afd8dc73e5f325028c2996359cbf35a814
snaptics11 <seo@snaptics.in>
2025-03-06 17:16:10 +0530

    updated new physiotheraphy content and images

--- src/components/physiotherapy/PhysiotherapyTabs.jsx
+++ src/components/physiotherapy/PhysiotherapyTabs.jsx
@@ -8,57 +8,81 @@ const PhysiotherapyTabs = () => {
 
   return (
     <>
-    <CommonBanner title={"Physiotherapy"} />
- 
-    <div className="container-lg">
+      <CommonBanner title={"Physiotherapy"} />
 
-      <div className="tabsContainerParent" style={styles.container}>
-        <div className="innerTabsContainer" style={styles.tabsContainer}>
-          {tabs.map((tab, index) => (
-            <button
-              key={index}
-              onClick={() => setActiveTab(index)}
-              style={activeTab === index ? styles.activeTab : styles.tab}
-            >
-              {tab.title}
-            </button>
-          ))}
-        </div>
+      <div className="container-lg">
+        <div className="tabsContainerParent" style={styles.container}>
+          <div className="innerTabsContainer" style={styles.tabsContainer}>
+            {tabs.map((tab, index) => (
+              <button
+                key={index}
+                onClick={() => setActiveTab(index)}
+                style={activeTab === index ? styles.activeTab : styles.tab}
+              >
+                {tab.title}
+              </button>
+            ))}
+          </div>
 
-        <div className="tabsContent-Conatiner" style={styles.contentContainer}>
-          <h3 style={styles.title}>{tabs[activeTab]?.title} : </h3>
-          {tabs[activeTab]?.content?.map((item, index) => {
-            // Get the content key dynamically (content1, content2, etc.)
-            const contentKey = `content${index + 1}`;
-            const content = item[contentKey];
+          <div
+            className="tabsContent-Conatiner"
+            style={styles.contentContainer}
+          >
+            <h3 style={styles.title}>{tabs[activeTab]?.title} : </h3>
+            {tabs[activeTab]?.content?.map((item, index) => {
+              // Get the content key dynamically (content1, content2, etc.)
+              const contentKey = `content${index + 1}`;
+              const content = item[contentKey];
 
-            return (
-              <div key={index}>
-                <div className="row align-items-center border p-3 mb-3 border-bottom">
-                  <div className="col-md-8">
-                    <h4
-                      style={{ color: "rgb(5, 145, 191)", fontWeight: "600" }}
-                    >
-                      {content?.title}
-                    </h4>
-                    <ul className="mt-4">
-                      {content?.text?.map((text, textIndex) => (
-                        <li key={textIndex}>{text}</li>
-                      ))}
-                    </ul>
-                  </div>
-                  <div className="col-md-4">
-                    <div className="excerciseImage">
-                      <img src={content?.image} alt={content?.title} />
+              return (
+                <div key={index}>
+                  <div className="row align-items-center border p-3 mb-3 border-bottom">
+                    <div className="col-md-8">
+                      <h4
+                        style={{
+                          color: "rgb(5, 145, 191)",
+                          fontWeight: "600",
+                          textTransform: "capitalize",
+                        }}
+                      >
+                        {content?.title}
+                      </h4>
+                      <ul className="mt-4">
+                        {content?.text?.map((text, textIndex) => (
+                          <li key={textIndex}>{text}</li>
+                        ))}
+                      </ul>
+                    </div>
+                    <div className="col-md-4">
+                      <div className="excerciseImage">
+                        <img
+                          src={content?.image}
+                          alt={content?.title}
+                          style={{ maxWidth: "100%", height: "auto" }}
+                        />
+                        {content?.image2 && (
+                          <img
+                            src={content.image2}
+                            alt={content?.title + " 2"}
+                            style={{ maxWidth: "100%", height: "auto" }}
+                          />
+                        )}
+                        {content?.image3 && (
+                          <img
+                            src={content.image3}
+                            alt={content?.title + " 3"}
+                            style={{ maxWidth: "100%", height: "auto" }}
+                          />
+                        )}
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
--- src/components/services/ServicePageTabs.jsx
+++ src/components/services/ServicePageTabs.jsx
@@ -5,79 +5,79 @@ const ServicePageTabs = () => {
     const [activeTab, setActiveTab] = useState('Knee Care');
     const data = [
         {
-            image: 'https://bmjtherapy.com/wp-content/uploads/2021/07/Singapore-physio-stretching-out-patients-inflammed-knee-scaled.jpg',
+            image: '/assets/images/peducation1.jpg',
             title: 'Exercises after a Total Knee Replacement',
             content: 'Effective exercises to aid recovery post knee replacement surgery.',
             category: 'Knee Care'
         },
         {
-            image: 'https://media.licdn.com/dms/image/D5612AQFt1RISmqkKUQ/article-cover_image-shrink_720_1280/0/1718690355415?e=2147483647&v=beta&t=jnXeLHY3Q9G4LeY8Un4zSea_mBXtwp5beA94NO5T-Mw',
+            image: '/assets/images/peducation2.jpeg',
             title: 'Managing Arthritis Pain',
             content: 'Lifestyle tips to ease arthritis discomfort.',
             category: 'Knee Care'
         },
         {
-            image: 'https://www.hiranandanihospital.org/public/uploads/blogs/1708066549.jpg',
+            image: '/assets/images/peducation3.jpg',
             title: 'Sports Injury Rehabilitation',
             content: 'Speed up your recovery with these tailored exercises.',
             category: 'Knee Care'
         },
         {
-            image: 'https://grandcarehealth.com/wp-content/uploads/2022/03/shutterstock_1231637074-scaled.jpg',
+            image: '/assets/images/peducation4.jpg',
             title: 'Post-Operative Care for Hip Replacement',
             content: 'Essential steps for a smooth hip surgery recovery.',
             category: 'Hip & Back'
         },
         {
-            image: 'https://www.baptisthealth.com/-/media/images/migrated/blog-images/teaser-images/gettyimages-802581672.jpg?rev=2e49d755597e495cb718f31e27e122f9',
+            image: '/assets/images/peducation5.jpg',
             title: 'Back Pain Relief Exercises',
             content: 'Simple exercises to alleviate chronic back pain.',
             category: 'Hip & Back'
         },
         {
-            image: 'https://sa1s3optim.patientpop.com/assets/images/provider/photos/2725198.jpeg',
+            image: '/assets/images/peducation6.jpeg',
             title: 'Managing Arthritis Pain',
             content: 'Effective exercises and lifestyle tips to manage arthritis pain.',
             category: 'Hip & Back'
         },
         {
-            image: 'https://media.istockphoto.com/id/1400845852/photo/one-active-mixed-race-man-from-the-back-stretching-arms-and-triceps-by-pulling-elbow-with.jpg?s=612x612&w=0&k=20&c=lErUOPno73j4RaHm82JUdRJ331IzyzrpVCSBBTcDEAM=',
+            image: '/assets/images/peducation7.jpg',
             title: 'Shoulder Mobility Exercises',
             content: 'Improve shoulder movement with targeted exercises.',
             category: 'Shoulder & Elbow'
         },
         {
-            image: 'https://parisorthopedic.com/wp-content/uploads/2020/05/Paris-Orthopedic_Osteoporosis-Treatments-1038x576.jpeg',
+            image: '/assets/images/peducation8.jpeg',
             title: 'Preventing Osteoporosis',
             content: 'Strengthen your bones with preventive exercises.',
             category: 'Shoulder & Elbow'
         },
         {
-            image: 'https://physioveda.ae/wp-content/uploads/2017/10/posture-correction-physioveda-2.jpg',
+            image: '/assets/images/peducation9.jpg',
             title: 'Posture Correction Tips',
             content: 'Maintain a healthy posture with easy-to-follow routines.',
             category: 'Shoulder & Elbow'
         },
         {
-            image: 'https://uoanj.com/wp-content/uploads/2024/05/UOA-ACL-Repair-vs-Reconstruction-Blog-min.png',
+            image: '/assets/images/peducation10.webp',
             title: 'ACL Injury Prevention',
             content: 'Prevent ACL injuries with these effective strengthening exercises.',
             category: 'Foot & Ankle'
         },
         {
-            image: 'https://cdn.shopify.com/s/files/1/0294/1911/0444/files/Blog_Post_Elbow_Joint_Pain_Treatement_Dr._Ortho_11_Jul_22_1.jpg?v=1657603529',
+            image: '/assets/images/peducation11.jpg',
             title: 'Elbow Pain Relief',
             content: 'Exercises to relieve elbow pain and improve mobility.',
             category: 'Shoulder & Elbow'
         },
         {
-            image: 'https://sa1s3optim.patientpop.com/assets/images/provider/photos/2119331.jpg',
+            image: '/assets/images/peducation12.jpg',
             title: 'Foot and Ankle Exercises',
             content: 'Maintain foot and ankle health with these simple exercises.',
             category: 'Foot & Ankle'
         },
         {
-            image: 'https://dance-teacher.com/wp-content/uploads/2020/12/980x-31.jpg',
+            image: '/assets/images/peducation13.jpg',
             title: 'Ankle Strengthening Exercises',
             content: 'Boost ankle stability and prevent injuries with these strengthening exercises.',
             category: 'Foot & Ankle'
@@ -109,7 +109,7 @@ const ServicePageTabs = () => {
                         <div className="row p-0 m-0">
                             {data
                                 .filter(item => item.category === activeTab)
-                                .slice(0, 3) // Display maximum 3 cards per tab
+                                .slice(0, 3) 
                                 .map((item, index) => (
                                     <div key={index} className="col-sm-6 col-md-4">
                                          <div className="education-card">
